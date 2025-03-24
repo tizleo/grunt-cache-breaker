051097aa98d2ea1b65c45d8add3ea99bdb4f6830
Tizian Leonhardt <leonhardt@pinacos.com>
2025-03-24 09:33:16 +0100

    fix: modernize cache_breaker

--- tasks/cache_breaker.js
+++ tasks/cache_breaker.js
@@ -1,39 +1,38 @@
 "use strict";
 
+const cb = require("cache-breaker");
+
 module.exports = function (grunt) {
 
     grunt.registerMultiTask("cachebreaker", "Rewrite links with timestamps or hashes", function () {
 
-        var options = this.options();
+        const options = this.options();
 
         if (!options.match) {
             return grunt.fail.warn("You must provide the 'match' option");
         }
 
-        var cb = require("cache-breaker");
-
-        this.files.forEach(function (f) {
+        this.files.forEach((f) => {
 
             if (!f.src.length) {
                 return grunt.fail.warn("No source files were found.");
             }
 
-            f.src.forEach(function (filepath) {
-
-                var dest = filepath;
+            f.src.forEach((filepath) => {
+                let dest = filepath;
 
                 if (f.dest && f.dest !== "src") {
                     dest = f.dest;
                 }
 
-                var input  = grunt.file.read(filepath);
+                let input = grunt.file.read(filepath);
 
                 if (options.replacement === "md5") {
-                    options.match.forEach(function (item) {
+                    options.match.forEach((item) => {
                         if (typeof item === "string") {
                             input = cb.breakCache(input, item, options);
                         } else {
-                            var clone = structuredClone(options);
+                            const clone = structuredClone(options);
                             Object.keys(item).forEach(function (key) {
                                 clone.src = {
                                     path: item[key]
